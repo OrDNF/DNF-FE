775515c9346616a2b91d5ee8604c34d67738fcfd
BaoNguyen <baokx2k3@gmail.com>
2024-10-09 15:08:45 +0700

    update

--- src/app/modules/auth/user/user.module.ts
+++ src/app/modules/auth/user/user.module.ts
@@ -4,13 +4,21 @@ import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { UserRoutingModule } from './user-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 
 
 
 @NgModule({
   declarations: [LoginComponent, ErrorComponent, RegisterComponent, ForgotpasswordComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class UserModule { }
